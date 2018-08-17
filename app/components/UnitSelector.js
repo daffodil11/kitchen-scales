15420729622a96c45b716d2ac7e975d0de3499df
Simon <simonr.davey@gmail.com>
2018-08-17 12:09:04 +0100

    Made Unitselector a Stateless Functional Component.

--- app/components/UnitSelector.js
+++ app/components/UnitSelector.js
@@ -1,39 +1,37 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
-class UnitSelector extends React.Component {
-  render() {
-    return (
-      <form>
-        <fieldset>
-          <legend>
-            <div>
-              <label htmlFor="metric">
-                Metric
-                <input
-                  type="radio"
-                  id="metric"
-                  name="unit"
-                  checked={this.props.selected==="metric"}
-                  onChange={this.props.onChange.bind(null, "metric")}/>
-              </label>
-            </div>
-            <div>
-              <label htmlFor="imperial">
-                Imperial
-                <input
-                  type="radio"
-                  id="imperial"
-                  name="unit"
-                  checked={this.props.selected==="imperial"}
-                  onChange={this.props.onChange.bind(null, "imperial")} />
-              </label>
-            </div>
-          </legend>
-        </fieldset>
-      </form>
-    )
-  }
+var UnitSelector = function (props) {
+  return (
+    <form>
+      <fieldset>
+        <legend>
+          <div>
+            <label htmlFor="metric">
+              Metric
+              <input
+                type="radio"
+                id="metric"
+                name="unit"
+                checked={props.selected==="metric"}
+                onChange={props.onChange.bind(null, "metric")}/>
+            </label>
+          </div>
+          <div>
+            <label htmlFor="imperial">
+              Imperial
+              <input
+                type="radio"
+                id="imperial"
+                name="unit"
+                checked={props.selected==="imperial"}
+                onChange={props.onChange.bind(null, "imperial")} />
+            </label>
+          </div>
+        </legend>
+      </fieldset>
+    </form>
+  )
 }
 UnitSelector.propTypes = {
   selected : PropTypes.string.isRequired,
